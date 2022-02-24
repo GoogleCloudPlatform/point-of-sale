754159312f86a1a24095a4a295e9222372984704
shabirmean <shabirmean@google.com>
2022-02-24 10:44:51 -0500

    cleanup: add correct node version tag

--- .github/cloudbuild/skaffold-builder/skaffold.bash
+++ .github/cloudbuild/skaffold-builder/skaffold.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 # Invoke kubectl.bash to process CLOUDSDK_* environment variables and make a
@@ -7,5 +21,5 @@ set -e
 
 gcloud auth configure-docker
 
-echo "Running: skaffold $@"
+echo "Running: skaffold $*"
 skaffold "$@"
