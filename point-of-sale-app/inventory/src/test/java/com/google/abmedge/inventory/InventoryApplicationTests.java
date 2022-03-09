a1dddc0a5e6e0da5012d494982e307f66e2d993d
shabirmean <shabirmean@google.com>
2022-03-09 18:17:14 -0500

    fix: NPE

--- point-of-sale-app/inventory/src/test/java/com/google/abmedge/inventory/InventoryApplicationTests.java
+++ point-of-sale-app/inventory/src/test/java/com/google/abmedge/inventory/InventoryApplicationTests.java
@@ -15,8 +15,6 @@
 package com.google.abmedge.inventory;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
