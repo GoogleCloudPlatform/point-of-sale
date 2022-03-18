33bd5a04fe8b72fbc83db4e440e7df13803398fe
shabirmean <shabirmean@google.com>
2022-03-18 15:53:13 -0400

    fix: error with the incorrect annotation

--- point-of-sale-app/service-sdk/src/main/java/com/google/abmedge/payment/PaymentUnit.java
+++ point-of-sale-app/service-sdk/src/main/java/com/google/abmedge/payment/PaymentUnit.java
@@ -39,6 +39,8 @@ public class PaymentUnit implements Serializable {
   public static final String PAYMENT_UNIT_TABLE = "payment_units";
 
   @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
   @Type(type = "org.hibernate.type.UUIDCharType")
   @Column(columnDefinition = "CHAR(36)")
   private UUID id = UUID.randomUUID();
@@ -63,7 +65,6 @@ public class PaymentUnit implements Serializable {
     this.totalCost = totalCost;
   }
 
-  @Id
   public UUID getId() {
     return id;
   }
