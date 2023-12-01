a1cd4ba48f9dd702b8a4552fcc1cd62d0c1e79cf
Aaron Wanjala <aaronmwanjala@google.com>
2023-12-01 18:49:24 +0000

    chore: run coveo format

--- src/inventory/src/test/java/com/google/abmedge/inventory/AbstractContainerDatabaseTest.java
+++ src/inventory/src/test/java/com/google/abmedge/inventory/AbstractContainerDatabaseTest.java
@@ -15,31 +15,31 @@ package com.google.abmedge.inventory;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-
-import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 
 public abstract class AbstractContainerDatabaseTest {
 
-    protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
-        DataSource ds = getDataSource(container);
-        Statement statement = ds.getConnection().createStatement();
-        statement.execute(sql);
-        ResultSet resultSet = statement.getResultSet();
+  protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql)
+      throws SQLException {
+    DataSource ds = getDataSource(container);
+    Statement statement = ds.getConnection().createStatement();
+    statement.execute(sql);
+    ResultSet resultSet = statement.getResultSet();
 
-        resultSet.next();
-        return resultSet;
-    }
+    resultSet.next();
+    return resultSet;
+  }
 
-    protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(container.getJdbcUrl());
-        hikariConfig.setUsername(container.getUsername());
-        hikariConfig.setPassword(container.getPassword());
-        hikariConfig.setDriverClassName(container.getDriverClassName());
-        return new HikariDataSource(hikariConfig);
-    }
+  protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setJdbcUrl(container.getJdbcUrl());
+    hikariConfig.setUsername(container.getUsername());
+    hikariConfig.setPassword(container.getPassword());
+    hikariConfig.setDriverClassName(container.getDriverClassName());
+    return new HikariDataSource(hikariConfig);
+  }
 }
--- src/inventory/src/test/java/com/google/abmedge/inventory/InventoryIntegrationTest.java
+++ src/inventory/src/test/java/com/google/abmedge/inventory/InventoryIntegrationTest.java
@@ -14,9 +14,14 @@
 
 package com.google.abmedge.inventory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
+import java.io.IOException;
+import java.net.InetAddress;
 import org.apache.logging.log4j.util.Strings;
 import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,139 +44,137 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.IOException;
-import java.net.InetAddress;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-
-
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryIntegrationTest extends AbstractContainerDatabaseTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(InventoryIntegrationTest.class);
+  private static final Logger logger = LoggerFactory.getLogger(InventoryIntegrationTest.class);
 
-    @Value("${local.server.port}")
-    private String localServerPort;
+  @Value("${local.server.port}")
+  private String localServerPort;
 
-    // Create a common docker network so that containers can communicate
-    @Rule
-    public static Network network = Network.newNetwork();
+  // Create a common docker network so that containers can communicate
+  @Rule public static Network network = Network.newNetwork();
 
-    static ToxiproxyClient toxiproxyClient;
+  static ToxiproxyClient toxiproxyClient;
 
-    static Proxy apiProxy;
+  static Proxy apiProxy;
 
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+  @Autowired private TestRestTemplate testRestTemplate;
 
-    static String proxyHostAndPort;
+  static String proxyHostAndPort;
 
-    // Toxiproxy container, which will be used as a TCP proxy
-    @Rule
-    @Container
-    public static ToxiproxyContainer toxiproxy = new ToxiproxyContainer("ghcr.io/shopify/toxiproxy:2.5.0")
-            .withNetwork(network);
+  // Toxiproxy container, which will be used as a TCP proxy
+  @Rule @Container
+  public static ToxiproxyContainer toxiproxy =
+      new ToxiproxyContainer("ghcr.io/shopify/toxiproxy:2.5.0").withNetwork(network);
 
-    @Rule
-    @Container
-    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:5.7.34")
-            .withNetwork(network)
-            .withNetworkAliases("mysql")
-            .withLogConsumer(new Slf4jLogConsumer(logger))
-            .withDatabaseName("pos_db")
-            .withUsername("testUser")
-            .withPassword("pass");
+  @Rule @Container
+  public static MySQLContainer<?> mySQLContainer =
+      new MySQLContainer<>("mysql:5.7.34")
+          .withNetwork(network)
+          .withNetworkAliases("mysql")
+          .withLogConsumer(new Slf4jLogConsumer(logger))
+          .withDatabaseName("pos_db")
+          .withUsername("testUser")
+          .withPassword("pass");
 
-    @DynamicPropertySource
-    static void registerTestProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url",
-                () -> String.format("jdbc:mysql://%s:%d/pos_db", toxiproxy.getHost(), toxiproxy.getFirstMappedPort() + 1));
-        registry.add("spring.datasource.username", () -> "testUser");
-        registry.add("spring.datasource.password", () -> "pass");
-        registry.add("spring.datasource.driverClassName", () -> "com.mysql.jdbc.Driver");
-        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-        registry.add("server.tomcat.connection-timeout", () -> "5000");
+  @DynamicPropertySource
+  static void registerTestProperties(DynamicPropertyRegistry registry) {
+    registry.add(
+        "spring.datasource.url",
+        () ->
+            String.format(
+                "jdbc:mysql://%s:%d/pos_db",
+                toxiproxy.getHost(), toxiproxy.getFirstMappedPort() + 1));
+    registry.add("spring.datasource.username", () -> "testUser");
+    registry.add("spring.datasource.password", () -> "pass");
+    registry.add("spring.datasource.driverClassName", () -> "com.mysql.jdbc.Driver");
+    registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
+    registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    registry.add("server.tomcat.connection-timeout", () -> "5000");
+  }
+
+  @BeforeEach
+  void initializeTestProxy() throws IOException {
+    if (toxiproxyClient == null) {
+      InetAddress hostRunningTheAppEndpoint = InetAddress.getLocalHost();
+      toxiproxyClient = new ToxiproxyClient(toxiproxy.getHost(), toxiproxy.getFirstMappedPort());
+      int toxiproxyPort = 8666;
+      apiProxy =
+          toxiproxyClient.createProxy(
+              "apiProxy",
+              "0.0.0.0:" + toxiproxyPort,
+              hostRunningTheAppEndpoint.getHostAddress() + ":" + localServerPort);
+      proxyHostAndPort =
+          String.format(
+              "http://%s:%d", toxiproxy.getHost(), toxiproxy.getMappedPort(toxiproxyPort));
     }
+  }
 
-    @BeforeEach
-    void initializeTestProxy() throws IOException {
-        if (toxiproxyClient == null) {
-            InetAddress hostRunningTheAppEndpoint = InetAddress.getLocalHost();
-            toxiproxyClient = new ToxiproxyClient(toxiproxy.getHost(), toxiproxy.getFirstMappedPort());
-            int toxiproxyPort = 8666;
-            apiProxy = toxiproxyClient.createProxy("apiProxy",
-                    "0.0.0.0:" + toxiproxyPort,
-                    hostRunningTheAppEndpoint.getHostAddress() + ":" + localServerPort);
-            proxyHostAndPort = String.format("http://%s:%d", toxiproxy.getHost(), toxiproxy.getMappedPort(toxiproxyPort));
-        }
-    }
+  @Test
+  void testConnectionCut() throws IOException {
+    ResponseEntity<String> itemsBeforeToxic =
+        this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
 
-    @Test
-    void testConnectionCut() throws IOException {
-        ResponseEntity<String> itemsBeforeToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+    assertThat(
+            itemsBeforeToxic.getStatusCode().equals(HttpStatus.OK)
+                && Strings.isNotBlank(itemsBeforeToxic.getBody()))
+        .as("we are able to read and write from the JPARepository before cutting the connection");
 
-        assertThat(
-                itemsBeforeToxic.getStatusCode().equals(HttpStatus.OK) &&
-                        Strings.isNotBlank(itemsBeforeToxic.getBody())
-        )
-                .as("we are able to read and write from the JPARepository before cutting the connection");
+    apiProxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
+    apiProxy.toxics().bandwidth("CUT_CONNECTION_UPSTREAM", ToxicDirection.UPSTREAM, 0);
 
-        apiProxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
-        apiProxy.toxics().bandwidth("CUT_CONNECTION_UPSTREAM", ToxicDirection.UPSTREAM, 0);
+    assertThat(
+            catchThrowable(
+                () -> {
+                  ResponseEntity<String> itemsDuringToxic =
+                      this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+                }))
+        .as("call should fail when the connection is cut")
+        .isInstanceOf(ResourceAccessException.class);
 
+    apiProxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
+    apiProxy.toxics().get("CUT_CONNECTION_UPSTREAM").remove();
 
-        assertThat(
-                catchThrowable(() -> {
-                    ResponseEntity<String> itemsDuringToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
-                })
-        )
-                .as("call should fail when the connection is cut")
-                .isInstanceOf(ResourceAccessException.class);
+    ResponseEntity<String> itemsAfterToxic =
+        this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+    assertThat(
+            itemsAfterToxic.getStatusCode().equals(HttpStatus.OK)
+                && Strings.isNotBlank(itemsAfterToxic.getBody()))
+        .as("access is restored once the network returns");
+  }
 
-        apiProxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
-        apiProxy.toxics().get("CUT_CONNECTION_UPSTREAM").remove();
+  @Test
+  void SecondTest() throws IOException {
+    ResponseEntity<String> itemsBeforeToxic =
+        this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
 
-        ResponseEntity<String> itemsAfterToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
-        assertThat(
-                itemsAfterToxic.getStatusCode().equals(HttpStatus.OK) &&
-                        Strings.isNotBlank(itemsAfterToxic.getBody())
-        )
-                .as("access is restored once the network returns");
-    }
+    assertThat(
+            itemsBeforeToxic.getStatusCode().equals(HttpStatus.OK)
+                && Strings.isNotBlank(itemsBeforeToxic.getBody()))
+        .as("we are able to read and write from the JPARepository before cutting the connection");
 
-    @Test
-    void SecondTest() throws IOException {
-        ResponseEntity<String> itemsBeforeToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+    apiProxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
+    apiProxy.toxics().bandwidth("CUT_CONNECTION_UPSTREAM", ToxicDirection.UPSTREAM, 0);
 
-        assertThat(
-                itemsBeforeToxic.getStatusCode().equals(HttpStatus.OK) &&
-                        Strings.isNotBlank(itemsBeforeToxic.getBody())
-        )
-                .as("we are able to read and write from the JPARepository before cutting the connection");
+    assertThat(
+            catchThrowable(
+                () -> {
+                  ResponseEntity<String> itemsDuringToxic =
+                      this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+                }))
+        .as("call should fail when the connection is cut")
+        .isInstanceOf(ResourceAccessException.class);
 
-        apiProxy.toxics().bandwidth("CUT_CONNECTION_DOWNSTREAM", ToxicDirection.DOWNSTREAM, 0);
-        apiProxy.toxics().bandwidth("CUT_CONNECTION_UPSTREAM", ToxicDirection.UPSTREAM, 0);
+    apiProxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
+    apiProxy.toxics().get("CUT_CONNECTION_UPSTREAM").remove();
 
-
-        assertThat(
-                catchThrowable(() -> {
-                    ResponseEntity<String> itemsDuringToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
-                })
-        )
-                .as("call should fail when the connection is cut")
-                .isInstanceOf(ResourceAccessException.class);
-
-        apiProxy.toxics().get("CUT_CONNECTION_DOWNSTREAM").remove();
-        apiProxy.toxics().get("CUT_CONNECTION_UPSTREAM").remove();
-
-        ResponseEntity<String> itemsAfterToxic = this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
-        assertThat(
-                itemsAfterToxic.getStatusCode().equals(HttpStatus.OK) &&
-                        Strings.isNotBlank(itemsAfterToxic.getBody())
-        )
-                .as("access is restored once the network returns");
-    }
+    ResponseEntity<String> itemsAfterToxic =
+        this.testRestTemplate.getForEntity(proxyHostAndPort + "/items", String.class);
+    assertThat(
+            itemsAfterToxic.getStatusCode().equals(HttpStatus.OK)
+                && Strings.isNotBlank(itemsAfterToxic.getBody()))
+        .as("access is restored once the network returns");
+  }
 }
